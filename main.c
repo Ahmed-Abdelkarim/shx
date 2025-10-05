04f2fbc0e1f1d110e4636321d81ff75be8038350
Ahmed Abdelkarim <ahmedabdelhafez20111@gmail.com>
2025-10-05 19:34:51 +0300

    inital commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cstdlib>
+
+int main(int argc, char *argv[]) {
+
+    // Initialize the shell
+
+    // loop to process commands
+    printf("Hello, World!\n");
+
+    // Cleanup and exit
+    return EXIT_SUCCESS;
+}
